940f97459fbd8d139d2d30cf1aba9b581386fdfb
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-09-24 16:33:38 +0200

    header file

---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 16:02:52 by alejhern          #+#    #+#             */
+/*   Updated: 2024/09/24 16:02:54 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+
+// void	free_array(char **ar);
+char	*find_path(char *cmd, char **envp);
+void	execute(char *argv, char **envp);
+void	error_exit(const char *msg);
+
+#endif
