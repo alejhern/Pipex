56243c2c2a53a75dc582af8dd3de32e33b74baf0
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2024-10-02 02:28:54 +0200

    get_next_line update

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: alejhern <alejhern@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 20:21:14 by alejhern          #+#    #+#             */
-/*   Updated: 2024/08/09 11:12:45 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/10/02 02:23:14 by amhernandez      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_buffer_join(char *s1, char *s2)
+static char	*ft_bufferjoin(char *s1, char *s2, int put_nl)
 {
 	size_t	t_size;
 	size_t	s1_size;
@@ -24,34 +24,30 @@ static char	*ft_buffer_join(char *s1, char *s2)
 	if (!s2)
 		return (s1);
 	s1_size = ft_strlen(s1);
-	t_size = s1_size + ft_strlen(s2) + 1;
+	t_size = s1_size + ft_strlen(s2) + 1 + put_nl;
 	s1 = (char *)ft_realloc(s1, t_size * sizeof(char));
 	if (!s1)
 		return (NULL);
 	while (*s2)
 		s1[s1_size++] = *s2++;
+	if (put_nl)
+		s1[s1_size++] = '\n';
 	return (s1);
 }
 
-static char	**buffer_check_nl(char **buffer, unsigned int buffer_ch)
+static char	**buffer_check_nl(char **buffer)
 {
-	buffer[3] = ft_strchr(buffer[buffer_ch], '\n');
-	if (buffer[3])
+	buffer[2] = ft_strchr(buffer[1], '\n');
+	if (buffer[2])
 	{
-		*buffer[3] = '\0';
-		buffer[0] = ft_buffer_join(buffer[0], buffer[buffer_ch]);
-		buffer[0] = ft_buffer_join(buffer[0], "\n");
-		buffer[3] = ft_strdup(buffer[3] + 1);
-		free(buffer[2]);
-		buffer[2] = ft_strdup(buffer[3]);
-		free(buffer[3]);
-		buffer[3] = NULL;
+		*buffer[2] = '\0';
+		buffer[0] = ft_bufferjoin(buffer[0], buffer[1], 1);
+		ft_memmove(buffer[1], buffer[2] + 1, ft_strlen(buffer[2] + 1) + 1);
 	}
 	else
 	{
-		buffer[0] = ft_buffer_join(buffer[0], buffer[buffer_ch]);
-		free(buffer[2]);
-		buffer[2] = NULL;
+		buffer[0] = ft_bufferjoin(buffer[0], buffer[1], 0);
+		*buffer[1] = '\0';
 	}
 	return (buffer);
 }
@@ -60,13 +56,10 @@ static char	**get_new_buffer(int fd, char **buffer)
 {
 	int	bytes_read;
 
-	if (buffer[2])
-	{
-		buffer_check_nl(buffer, 2);
-		if (buffer[2])
-			return (buffer);
-	}
-	while (1)
+	buffer_check_nl(buffer);
+	if (buffer[2] || !buffer[0])
+		return (buffer);
+	while (!buffer[2])
 	{
 		bytes_read = read(fd, buffer[1], BUFFER_SIZE);
 		if (bytes_read < 0)
@@ -78,23 +71,24 @@ static char	**get_new_buffer(int fd, char **buffer)
 		buffer[1][bytes_read] = '\0';
 		if (bytes_read == 0)
 			break ;
-		buffer_check_nl(buffer, 1);
-		if (buffer[2])
-			break ;
+		buffer_check_nl(buffer);
 	}
 	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer[1024][4];
+	static char	*buffer[1024][4] = {{NULL}};
 
 	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer[fd][1] = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	if (!buffer[fd][1])
-		return (NULL);
-	buffer[fd][0] = NULL;
+	{
+		buffer[fd][1] = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+		if (!buffer[fd][1])
+			return (NULL);
+	}
+	buffer[fd][0] = ft_strdup("");
 	get_new_buffer(fd, buffer[fd]);
 	if (buffer[fd][0] && buffer[fd][0][0] == '\0')
 	{
@@ -103,9 +97,8 @@ char	*get_next_line(int fd)
 	}
 	if (!buffer[fd][0])
 	{
-		free(buffer[fd][2]);
-		buffer[fd][2] = NULL;
+		free(buffer[fd][1]);
+		buffer[fd][1] = NULL;
 	}
-	free(buffer[fd][1]);
 	return (buffer[fd][0]);
 }
